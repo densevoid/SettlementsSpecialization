febb600fda7fdfce6390eb0456eef89ec87e7770
Densevoid <densevoid@yandex.ru>
2020-11-18 06:16:39 +0300

    Testing incident

---
+++ Source/SettlementsSpecialization/Events/IncidentWorker_TestIncident.cs
@@ -0,0 +1,38 @@
+﻿
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace SettlementsSpecialization.Events
+{
+    public class IncidentWorker_TestIncident : IncidentWorker
+    {
+        protected override bool CanFireNowSub(IncidentParms parms)
+        {
+            Map map = (Map)parms.target;
+            if (map.mapPawns.ColonistsSpawnedCount < 5)
+            {
+                Log.Message($"Colonists count: {map.mapPawns.ColonistsSpawnedCount}");
+                return false;
+            }
+            return true;
+        }
+
+        protected override bool TryExecuteWorker(IncidentParms parms)
+        {
+            Map map = (Map)parms.target;
+            if (map.mapPawns.ColonistsSpawnedCount < 5)
+            {
+                Log.Message($"Colonists count: {map.mapPawns.ColonistsSpawnedCount}");
+                return false;
+            }
+
+            Log.Message("EXECUTE!!!");
+
+            return true;
+        }
+    }
+}
